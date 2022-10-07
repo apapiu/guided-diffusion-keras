34e75a3cebccde78bd5f3b260b48f43a0750f441
Alexandru Papiu <apapiu@users.noreply.github.com>
2022-10-06 22:18:20 -0400

    add code for masked reverse diffusion for inpainting

--- guided_diffusion/diffuser.py
+++ guided_diffusion/diffuser.py
@@ -54,7 +54,7 @@ class Diffuser:
 
         return x0_pred
 
-    def reverse_diffusion(self, seeds, label, show_img=False, masked_imgs=None, mask=None, u=1):
+    def reverse_diffusion(self, seeds, label, show_img=False):
         """Reverse Guided Diffusion on a matrix of random images (seeds). Returns generated images"""
 
         new_img = seeds
@@ -68,6 +68,53 @@ class Diffuser:
 
             # new image at next_noise level is a weighted average of old image and predicted x0:
             new_img = ((curr_noise - next_noise) * x0_pred + next_noise * new_img) / curr_noise
+
+            if show_img:
+                plot_images(x0_pred, nrows=np.sqrt(len(new_img)),
+                            save_name=str(i),
+                            size=12)
+                plt.show()
+
+        return x0_pred
+
+    def reverse_diffusion_masked(self, seeds, label_ohe, show_img=False, masked_imgs=None, mask=None, u=1):
+        """Reverse Guided Diffusion on a matrix of random images (seeds) with a mask. Can be used for in/outpainting
+        Based on the algorithm from Repaint: https://github.com/andreas128/RePaint
+        """
+
+        new_img = seeds
+        num_imgs = len(new_img)
+
+        for i in tqdm(range(len(self.noise_levels) - 1)):
+
+            curr_noise, next_noise = self.noise_levels[i], self.noise_levels[i + 1]  # alpha1, alpha2
+
+            for j in range(1, u + 1):
+
+                # predict original denoised image:
+                x0_pred = self.predict_x_zero(new_img, label_ohe, curr_noise)
+
+                # new image at next_noise level is a weighted average of old image and predicted x0:
+                new_img = ((curr_noise - next_noise) * x0_pred + next_noise * new_img) / curr_noise
+
+                if masked_imgs is not None:
+                    # let the last 20% of steps be done without masking.
+                    if i <= int(self.diffusion_steps * 0.8):
+                        ####masked part:
+                        new_img_known = (1 - next_noise) * masked_imgs + np.random.normal(0, 1,
+                                                                                          size=new_img.shape) * next_noise
+
+                        # mask here is empty/unknown part
+                        new_img = new_img_known * mask + new_img * (1 - mask)
+                    else:
+                        break
+
+                if j != u:
+                    print(j)
+                    ### noise the image back to the previous step:
+                    s = (1 - curr_noise) / (1 - next_noise)
+                    new_img = s * new_img + np.sqrt(curr_noise ** 2 - s ** 2 * next_noise ** 2) * np.random.normal(0, 1,
+                                                                                                                   size=new_img.shape)
 
             if show_img:
                 plot_images(x0_pred, nrows=np.sqrt(len(new_img)),
